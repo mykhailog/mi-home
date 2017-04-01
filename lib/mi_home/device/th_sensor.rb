4a01adebb73851790da17a3281d322286842ba33
Mykhailo <mykhaylo@tactile.com>
2017-04-01 10:52:14 +0300

    fixed temperature/humidity sensor calculation

--- lib/mi_home/device/th_sensor.rb
+++ lib/mi_home/device/th_sensor.rb
@@ -4,8 +4,8 @@ module MiHome
       device_name 'Temperature And Humidity sensor'
       device_model :sensor_ht
 
-      property(:temperature) {|data| data[:temperature] / 100.0}
-      property(:humidity)    {|data| data[:humidity] / 100.0}
+      property(:temperature) {|data| data[:temperature].to_f / 100.0}
+      property(:humidity)    {|data| data[:humidity].to_f / 100.0}
     end
   end
 end
