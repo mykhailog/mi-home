b1f8e5c2af3b9b063330e908cf7bacefa022a1b4
Mykhailo <mykhaylo@tactile.com>
2017-04-09 11:59:33 +0300

    Fixed require order issue again. Forgot to include fix

--- lib/mi_home.rb
+++ lib/mi_home.rb
@@ -3,6 +3,7 @@ require 'openssl'
 require 'unobservable'
 require 'active_support/all'
 require_relative 'mi_home/version'
+require_relative './mi_home/device/aqara_device'
 Dir[File.dirname(__FILE__) + '/mi_home/device/*.rb'].each { |file| require file }
 require_relative './mi_home/aqara/aqara_platform'
 require_relative './mi_home/aqara/device_manager'
